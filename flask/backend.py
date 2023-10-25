c9bfb1a41dceacedea8ea5b1ea3f6de036e893fe
Anmolrana1 <125668969+Anmolrana1@users.noreply.github.com>
2023-10-25 14:58:35 +0530

    Update backend.py

--- flask/backend.py
+++ flask/backend.py
@@ -1,5 +1,6 @@
 activate_this = '/c/Users/dell/Desktop/projects/moviezone/activate_this.py'
-
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
 
 import pickle
 from flask import Flask, request, jsonify
