fb77934e34b59c6d1586c88848aaa43b9e652b41
Anmolrana1 <125668969+Anmolrana1@users.noreply.github.com>
2023-10-26 11:20:57 +0530

    Update backend.py

--- flask/backend.py
+++ flask/backend.py
@@ -1,4 +1,4 @@
-activate_this = '/c/Users/dell/Desktop/projects/moviezone/flask/backend.py'
+activate_this = 'venv/bin/activate_this.py'
 with open(activate_this) as file_:
     exec(file_.read(), dict(__file__=activate_this))
 
@@ -42,7 +42,7 @@ def recommend_movies(movie_name, movies_list, similarity):
         return {"error": str(e)}
 if __name__ == '__main__':
     app.run(debug=True)
-    deactivate_this =  '/c/Users/dell/Desktop/projects/moviezone/flask/backend.py'
+    deactivate_this ='venv/bin/activate_this.py'
     with open(deactivate_this) as file_:
         exec(file_.read(), dict(__file__=deactivate_this))
 
