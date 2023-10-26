8e698c28df49723a1dff9a4dc0110ace609c6ee9
Anmolrana1 <125668969+Anmolrana1@users.noreply.github.com>
2023-10-26 13:53:14 +0530

    Update backend.py

--- flask/backend.py
+++ flask/backend.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"/recommend": {"origins": "https://main--movielover.netlify.app"}})
 
 movies_list = pickle.load(open('movie_list.pkl', 'rb'))
 similarity = pickle.load(open('similarity.pkl', 'rb'))
