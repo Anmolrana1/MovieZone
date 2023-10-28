b0fbe8c32b450532e30838510d2b22f3bbd15d70
Anmolrana1 <125668969+Anmolrana1@users.noreply.github.com>
2023-10-28 23:00:22 +0530

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react"
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton"
+import 'react-loading-skeleton/dist/skeleton.css'
 import "./Card.css"
 import { Link } from "react-router-dom"
 
@@ -10,7 +11,7 @@ const Cards = ({movie}) => {
     useEffect(() => {
         setTimeout(() => {
             setIsLoading(false)
-        },1200)
+        },1000)
     }, []) 
 
     return <>
@@ -18,14 +19,14 @@ const Cards = ({movie}) => {
         isLoading
         ?
         <div className="cards">
-            <SkeletonTheme color="#202020" highlightColor="#444">
-                <Skeleton height={300} duration={2} count={3}/>
-            </SkeletonTheme>
+           <SkeletonTheme  highlightColor="rgb(194, 187, 187)" baseColor="rgb(43, 36, 36)" >
+                <Skeleton height={300}  />
+           </SkeletonTheme>
         </div>
         :
         <Link to={`/movies/${movie.id}`} style={{textDecoration:"none", color:"white"}}>
             <div className="cards">
-                <img className="cards__img" alt=" " src={`https://image.tmdb.org/t/p/original${movie?movie.poster_path:""}`} />
+                <img className="cards__img" alt="" src={`https://image.tmdb.org/t/p/original${movie?movie.poster_path:""}`} />
                 <div className="cards__overlay">
                     <div className="card__title">{movie?movie.original_title:""}</div>
                     <div className="card__runtime">
